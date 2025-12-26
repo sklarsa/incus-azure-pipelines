2f4596ec3e3e0313f3aba35d366efdf5e37c49b2
Steven Sklar <sklarsa@gmail.com>
2025-12-26 12:47:03 -0500

    starting to build reaper

---
+++ reaper.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	incus "github.com/lxc/incus/v6/client"
+	"github.com/lxc/incus/v6/shared/api"
+)
+
+func runReaper(ctx context.Context, c incus.InstanceServer, runFrequency time.Duration) {
+
+}
+
+func (r reaper) check(ctx context.Context, idx int) error {
+
+	op, err := r.c.ExecInstance(
+		agentName(idx),
+		api.InstanceExecPost{
+			Command: []string{
+				"/bin/bash",
+				"-c",
+				fmt.Sprintf("ps -u %s | grep run_agent.sh", agentUser),
+			},
+			WaitForWS:   true,
+			Interactive: false,
+		},
+		&incus.InstanceExecArgs{},
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := op.WaitContext(ctx); err != nil {
+		return err
+	}
+
+	meta := op.Get().Metadata
+	if meta == nil {
+		return fmt.Errorf("unable to obtain return code. metadata is nil")
+	}
+
+	returnCode, ok := meta["return"].(float64)
+	if ok {
+		exitCode := int(returnCode)
+		// if command succeeds, run_agent is still running and no reaping required
+		if exitCode == 0 {
+			return nil
+		}
+	} else {
+		return fmt.Errorf("unable to obtain return code. return code not found in metadata")
+	}
+
+	// If command returns a non-0, run_agent is not running (or ps has failed)
+	// so we need to reap the instance.
+	// First, let's lock the instance to ensure that we don't create it during
+	// the reaping process
+	if _, exists := inFlight.LoadOrStore(idx, true); exists {
+		// If the instance is inFlight, we don't need to do anything because
+		// it is already being re-created
+		return nil
+	}
+	defer inFlight.Delete(idx)
+
+}
